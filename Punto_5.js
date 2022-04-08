69fe7714d3b5b61c54d81d0b86445c2395cea757
Orantoon <71191565+Orantoon@users.noreply.github.com>
2022-04-07 22:13:04 -0600

    Quiz #8

---
+++ Punto_5.js
@@ -0,0 +1,3 @@
+// 5. Haga un programa en nodejs que haga un find de todas las atracciones en el cluster,
+// find de los paquetes de un país, y find de los operadores. Asegúrese que el connection
+// tenga presente los dos routers para que haya tolerancia fallas.
